937fa6cb86803764f83798e60468bf975b527f4b
fomakin <funtikswin@gmail.com>
2018-11-01 22:46:19 +0500

    modify

--- app/src/main/java/com/home/fomakin/roomwordsample/domain/WordRoomDatabase.java
+++ app/src/main/java/com/home/fomakin/roomwordsample/domain/WordRoomDatabase.java
@@ -11,9 +11,16 @@ import android.support.annotation.NonNull;
 import com.home.fomakin.roomwordsample.domain.dao.WordDao;
 import com.home.fomakin.roomwordsample.domain.entity.Word;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 @Database(entities = {Word.class}, version = 1)
 public abstract class WordRoomDatabase extends RoomDatabase {
 
+    private static final String DATABASE_NAME = "test.db";
     public abstract WordDao wordDao();
 
     private static volatile WordRoomDatabase INSTANCE;
@@ -26,13 +33,33 @@ public abstract class WordRoomDatabase extends RoomDatabase {
         }
     };
 
+    private static void copyDatabase(Context context) {
+        File dbPath = context.getDatabasePath(DATABASE_NAME);
+        if (!dbPath.exists()) {
+            try (InputStream is = context.getAssets().open("databases/" + DATABASE_NAME)) {
+                try (OutputStream os = new FileOutputStream(dbPath)) {
+                    byte[] buffer = new byte[8192];
+                    int length;
+                    while ((length = is.read(buffer, 0, 8192)) > 0) {
+                        os.write(buffer, 0, length);
+                    }
+                    os.flush();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     static WordRoomDatabase getDatabase(final Context context) {
         if (INSTANCE == null) {
             synchronized (WordRoomDatabase.class) {
                 if (INSTANCE == null) {
+                    copyDatabase(context);
                     // Create database
                     INSTANCE = Room
-                            .databaseBuilder(context.getApplicationContext(), WordRoomDatabase.class, "word_database")
+                            //.databaseBuilder(context.getApplicationContext(), WordRoomDatabase.class, "word_database")
+                            .databaseBuilder(context.getApplicationContext(), WordRoomDatabase.class, DATABASE_NAME)
                             .addCallback(roomDatabaseCallback)
                             .build();
                 }
@@ -51,11 +78,11 @@ public abstract class WordRoomDatabase extends RoomDatabase {
 
         @Override
         protected Void doInBackground(final Void... params) {
-            wordDao.deleteAll();
+            /*wordDao.deleteAll();
             Word word = new Word("Hello");
             wordDao.insert(word);
             word = new Word("World");
-            wordDao.insert(word);
+            wordDao.insert(word);*/
             return null;
         }
     }
